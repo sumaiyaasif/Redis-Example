9a5c3bb82414c073d5a5acf284cffb1122036bb4
sumaiyaasif <sumaiya.aa@gmail.com>
2020-03-16 21:35:59 -0500

    dotnet core application configured to local redis instance

---
+++ Redis-Example/Controllers/RedisController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Distributed;
+
+namespace Redis_Example
+{
+    [Route("api/[controller]")]
+    public class RedisController : Controller
+    {
+        private readonly IDistributedCache distributedCache; 
+        public RedisController(IDistributedCache distributedCache)
+        {
+            this.distributedCache = distributedCache;
+        }
+
+        [HttpGet]
+        public string Get()
+        {
+            return "Please enter a key at the end of the URL";
+        }
+
+
+        [HttpGet("{someKey}")]
+        public async Task<string> Get(string someKey)
+        {
+            return await GetValue(someKey);
+        }
+
+        private async Task<string> GetValue(string someKey)
+        {
+            var cacheKey = someKey.ToLower();
+
+            //var encodedMovies = await distributedCache.GetAsync(cacheKey);
+
+            
+                var value = Encoding.UTF8.GetBytes("someValue");
+                var options = new DistributedCacheEntryOptions()
+                                .SetSlidingExpiration(TimeSpan.FromMinutes(5))
+                                .SetAbsoluteExpiration(DateTime.Now.AddHours(6));
+                await distributedCache.SetAsync(cacheKey, value, options);
+            
+            return value.ToString();
+        }
+    }
+}
